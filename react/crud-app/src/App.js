3ed4b047f43f9b0735e78feb24261689800c94ce
rsyahbudin <syahbudin45@gmail.com>
2023-09-01 21:31:20 +0700

    update app.js

--- react/crud-app/src/App.js
+++ react/crud-app/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddUser } from "./components/AddUser";
+import { Edituser } from "./components/EditUser";
+import { GlobalProvider } from "./context/GlobalState";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div style={{ maxWidth: "30rem", margin: "4rem auto" }}>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+            {/* For route */}
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/add" component={AddUser}></Route>
+            <Route exact path="/edit/:id" component={Edituser}></Route>
+          </Switch>
+        </Router>
+      </GlobalProvider>
+    </div>
+  );
 };
 
 export default App;
